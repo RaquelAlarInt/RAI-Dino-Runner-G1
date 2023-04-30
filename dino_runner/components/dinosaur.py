6be6d5ca6dd19e412900ee1b08bf41992e086e4f
RaquelAlarInt <raquelalarint@gmail.com>
2023-04-29 20:22:24 -0400

    Class1-Tarea-Dino
    Implemente la accion de ducking, sin fallas, codigo completado!

--- dino_runner/components/dinosaur.py
+++ dino_runner/components/dinosaur.py
@@ -1,49 +1,69 @@
 import pygame
 from pygame.sprite import Sprite
-
-from dino_runner.utils.constants import RUNNING
+from dino_runner.utils.constants import DUCKING, JUMPING, RUNNING
 
 JUMP_VELOCITY = 8.5
+DUCKING_VELOCITY = 8.5
+
 DINO_RUNNING = "running"
 DINO_JUMPING = "jumping"
-
+DINO_DUCKING = "ducking"
 
 class Dinosaur(Sprite):
     POS_X = 80
     POS_Y = 310
     def __init__(self):
-        self.image = RUNNING[0]
+        self.image = RUNNING [0]
         self.rect = self.image.get_rect()
         self.rect.x = self.POS_X
         self.rect.y = self.POS_Y
+        
 
         self.step = 0
         self.action = DINO_RUNNING
         self.jump_velocity = JUMP_VELOCITY
+        self.ducking_velocity = DUCKING_VELOCITY
 
     def update(self, user_input):
         if self.action == DINO_RUNNING:
             self.run()
-        elif self.action ==DINO_JUMPING:
+        elif self.action == DINO_JUMPING:
             self.jump()
-        
+        elif self.action == DINO_DUCKING:
+            self.ducking()
+
         if self.action != DINO_JUMPING:
             if user_input[pygame.K_UP]:
                 self.action = DINO_JUMPING
+            elif user_input[pygame.K_DOWN]:
+                self.action = DINO_DUCKING
             else:
-                self.action =DINO_RUNNING
+                self.action = DINO_RUNNING
+       
 
         if self.step >= 10:
             self.step = 0
             
     def jump(self):
+        self.image =JUMPING
         self.rect.y -= self.jump_velocity *4
         self.jump_velocity -= 0.8
-        if self.jump_velocity < -8.5 :
+        if self.jump_velocity < -JUMP_VELOCITY:
             self.rect.y = self.POS_Y
             self.action = DINO_RUNNING
             self.jump_velocity = JUMP_VELOCITY
-            
+
+    def ducking(self):
+        self.image =DUCKING[0]
+        self.rect.y -= self.ducking_velocity *2
+        self.jump_velocity -= 0.8
+        if self.jump_velocity > -DUCKING_VELOCITY:
+            self.rect.y = self.POS_Y
+            self.action = DINO_RUNNING
+            self.jump_velocity = DUCKING_VELOCITY
+
+
+
     def run(self):
         self.image = RUNNING[0] if self.step < 5 else RUNNING[1]
         self.step += 1
